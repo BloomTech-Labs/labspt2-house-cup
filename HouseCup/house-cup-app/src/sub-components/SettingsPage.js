d2e84d0ff63f081ae7e57b44573eb644e97874bf
louislee7 <7.louislee@gmail.com>
2019-05-16 12:42:18 -0700

    settings page ui fixed

--- HouseCup/house-cup-app/src/sub-components/SettingsPage.js
+++ HouseCup/house-cup-app/src/sub-components/SettingsPage.js
@@ -4,82 +4,82 @@ import auth from '../utils/Auth';
 import axios from 'axios';
 
 class SettingsPage extends Component {
-					constructor(props) {
-					super(props);
-					this.state ={
-							email:'',
-							password: '',
-							newPassword:'',
-					};
-		}
+	constructor(props) {
+		super(props);
+		this.state = {
+			email: '',
+			password: '',
+			newPassword: '',
+		};
+	}
 
-componentDidMount() {
+	componentDidMount() {
 
-}
+	}
 
-updateUser = (newPassword) => {
+	updateUser = (newPassword) => {
 		const { getAccessToken } = auth;
 		console.log(`New password`, newPassword);
 		const headers = { Authorization: `Bearer ${getAccessToken()}` };
-		axios.patch('http://localhost:5000/users/update', newPassword, {headers})
-				.then( res=> {
-							console.log(`settings line 24`, res);
-				})
-				.catch(err => {
-					  	console.log(`Line 29 settingspage error`,err);
-				});  
-};
-
-handleInput = (event) => {
-this.setState({
-		[event.target.name]: event.target.value
-    })
-}
-
-handleSubmit = (event) => {
-			event.preventDefault();
-			const updatedUser =  {password: this.state.newPassword}
-			this.updateUser(updatedUser)
-			this.setState({
-					email: '',
-					password: '',
-					newPassword: ''
+		axios.patch('http://localhost:5000/users/update', newPassword, { headers })
+			.then(res => {
+				console.log(`settings line 24`, res);
+			})
+			.catch(err => {
+				console.log(`Line 29 settingspage error`, err);
 			});
-  }
-render() {
-return (
-<>
-<SideMenu />
-<div className="settings">
-<div className="Modal">
-<form onSubmit={this.handleSubmit}>
-	<h1>Update Password</h1>
-            <input name="email"
-						       type="email"
-									 className="Input" 
-									 placeholder="Your email"
-									 value={this.state.email}
-									 onChange={this.handleInput} required />
-            <input name="password"
-						       type="password" 
-									 className="Input" 
-									 placeholder="Old Password"
-									 value={this.state.password}
-									 onChange={this.handleInput} required />
-            <input name="newPassword"
-						       type="password" 
-									 className="Input" 
-									 placeholder="New Password"
-									 value={this.state.newPassword}
-									 onChange={this.handleInput} required  />		
-					  <button type="submit" value="Save">Save</button>			 							 									 
-</form>
+	};
 
+	handleInput = (event) => {
+		this.setState({
+			[event.target.name]: event.target.value
+		})
+	}
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+		const updatedUser = { password: this.state.newPassword }
+		this.updateUser(updatedUser)
+		this.setState({
+			email: '',
+			password: '',
+			newPassword: ''
+		});
+	}
+	render() {
+		return (
+			<div className="settings-page">
+				<SideMenu />
+				<div className="settings">
+					<div>
+						<h2 className="settings-title">Update Your Info</h2>
 					</div>
+					<form className="settings-input-container" onSubmit={this.handleSubmit}>
+						<input name="email"
+							type="email"
+							className="Input"
+							placeholder="Your email"
+							value={this.state.email}
+							onChange={this.handleInput} required />
+						<input name="password"
+							type="password"
+							className="Input"
+							placeholder="Old Password"
+							value={this.state.password}
+							onChange={this.handleInput} required />
+						<input name="newPassword"
+							type="password"
+							className="Input"
+							placeholder="New Password"
+							value={this.state.newPassword}
+							onChange={this.handleInput} required />
+						<button className="save-button" type="submit" value="Save">Save</button>
+					</form>
+
 				</div>
-			</>
+			</div>
 		);
- }
+	}
 }
 
 export default SettingsPage;
