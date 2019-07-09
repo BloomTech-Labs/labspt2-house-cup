c42248c908e0d5b49536551777eeb40a912e20e4
vy3191 <venkyafhs@gmail.com>
2019-07-09 18:36:08 -0400

    Modified user_middleware.js file

--- server/middleware/user_middleware.js
+++ server/middleware/user_middleware.js
@@ -1,16 +1,11 @@
 const { User } = require('../Models');
-const  request = require("superagent");
+const request = require("superagent");
 require('dotenv').config()
 
 const NON_INTERACTIVE_CLIENT_ID = "s9Qekd0RGrf7q6MzklrKi8n6R9HzKz06";
 const NON_INTERACTIVE_CLIENT_SECRET = "09m5DSGrksPQf7WQq5OS3uTqY11l6RmLufxUGVoWvusF3Kf52Tb1ZUiE9UBOEXmO";
 
-const authCredentials = {
-  client_id: NON_INTERACTIVE_CLIENT_ID,
-  client_secret: NON_INTERACTIVE_CLIENT_SECRET,
-  grant_type: 'client_credentials',
-  audience: "https://venky-yagatilee.auth0.com/api/v2/"
-};
+
 const authData = {
   client_id: NON_INTERACTIVE_CLIENT_ID,
   client_secret: NON_INTERACTIVE_CLIENT_SECRET,
